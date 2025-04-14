2de779bfce09b69f1a3ad409a51356aa24430749
yossi842 <139813057+yossi842@users.noreply.github.com>
2025-04-14 15:17:39 +0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,5 @@
+function search() {
+    const searchTerm = document.getElementById("searchInput").value;
+    // כאן אנחנו נשלח את ה-searchTerm לשרת
+    console.log("הולך לשלוח לשרת:", searchTerm);
+}
